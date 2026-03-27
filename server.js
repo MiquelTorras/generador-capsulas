7f01460c19d5d0fb50d41964294ceaf0e1fb5169
Miquel Torras <mtorras.ep@gmail.com>
2026-03-27 17:35:55 +0100

    fix cors preflight definitivo

--- server.js
+++ server.js
@@ -7,10 +7,17 @@ console.log('🚀 SERVER NUEVO CARGADO')
 const app = express()
 
 app.use(cors({
-  origin: '*',
+  origin: 'https://www.homomobilis.net',
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type']
 }))
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://www.homomobilis.net')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 
 app.use(express.json())
 app.use(express.static('public'))
